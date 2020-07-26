2d77a96e16b6b2847d831fef930d49790097626a
shajie <shajieshah@gmail.com>
2020-07-26 21:40:03 +0500

    Sample App

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,67 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = current_user.posts
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def edit
+  end
+
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html do
+         redirect_to @post, notice: 'Post was successfully created.' 
+       end
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html do 
+          render :new 
+          puts @post.errors.full_messages #display errors message 
+        end
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :description , :visible)
+    end
+end
